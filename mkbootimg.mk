3562104a3a055039a0be56f63e28033b18082e28
Menno van Grinsven <scanno71@gmail.com>
2016-10-14 10:21:01 +0200

    Update dtbtool from CM
    Change-Id: I6499101d74f3cf4e0c0dc6968e2cbb0fb3b8e063

--- mkbootimg.mk
+++ mkbootimg.mk
@@ -1,45 +1,39 @@
 LOCAL_PATH := $(call my-dir)
 
-## Don't change anything under here. The variables are named MSM8916_whatever
-## on purpose, to avoid conflicts with similarly named variables at other
-## parts of the build environment
-
-## Imported from the original makefile...
-KERNEL_CONFIG := $(KERNEL_OUT)/.config
-KERNEL_ZIMG = $(KERNEL_OUT)/arch/arm/boot/zImage
-DTC = $(KERNEL_OUT)/scripts/dtc/dtc
-
-
 ## Build and run dtbtool
-DTBTOOL := $(HOST_OUT_EXECUTABLES)/dtbToolCM$(HOST_EXECUTABLE_SUFFIX)
-
+DTBTOOL := $(HOST_OUT_EXECUTABLES)/dtbTool.clark$(HOST_EXECUTABLE_SUFFIX)
 INSTALLED_DTIMAGE_TARGET := $(PRODUCT_OUT)/dt.img
 
-ifndef TARGET_PREBUILT_DTB
 $(INSTALLED_DTIMAGE_TARGET): $(DTBTOOL) $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr $(INSTALLED_KERNEL_TARGET)
-	@echo -e ${CL_CYN}"Start DT image: $@"${CL_RST}
 	$(call pretty,"Target dt image: $(INSTALLED_DTIMAGE_TARGET)")
-	$(hide) $(DTBTOOL) -o $(INSTALLED_DTIMAGE_TARGET) -s $(BOARD_KERNEL_PAGESIZE) -p $(KERNEL_OUT)/scripts/dtc/ $(KERNEL_OUT)/arch/arm/boot/dts/qcom/
-	@echo -e ${CL_CYN}"Made DT image: $@"${CL_RST}
-else
-$(INSTALLED_DTIMAGE_TARGET): $(TARGET_PREBUILT_DTB)
-	cp $(TARGET_PREBUILT_DTB) $(INSTALLED_DTIMAGE_TARGET)
-endif
+	$(hide) $(DTBTOOL) -o $(INSTALLED_DTIMAGE_TARGET) -s $(BOARD_KERNEL_PAGESIZE) -p $(KERNEL_OUT)/scripts/dtc/ $(KERNEL_OUT)/arch/$(KERNEL_ARCH)/boot/dts/
+	@echo "Made DT image: $@"
+
 
 ## Overload bootimg generation: Same as the original, + --dt arg
-$(INSTALLED_BOOTIMAGE_TARGET): $(MKBOOTIMG) $(INTERNAL_BOOTIMAGE_FILES) $(INSTALLED_DTIMAGE_TARGET)
+$(INSTALLED_BOOTIMAGE_TARGET): $(MKBOOTIMG) $(INTERNAL_BOOTIMAGE_FILES) $(INSTALLED_DTIMAGE_TARGET) $(PRODUCT_OUT)/utilities/toybox
 	$(call pretty,"Target boot image: $@")
+	@echo "----- Copying static toybox to ramdisk ------"
+	$(hide) mkdir -p $(PRODUCT_OUT)/root/sbin/static
+	$(hide) cp $(PRODUCT_OUT)/utilities/toybox $(PRODUCT_OUT)/root/sbin/static/toybox
+	@echo "----- Making boot ramdisk ------"
+	$(hide) rm -f $(INSTALLED_RAMDISK_TARGET)
+	$(hide) $(MKBOOTFS) $(TARGET_ROOT_OUT) | $(MINIGZIP) > $(INSTALLED_RAMDISK_TARGET)
+	@echo "----- Making boot image ------"
 	$(hide) $(MKBOOTIMG) $(INTERNAL_BOOTIMAGE_ARGS) $(BOARD_MKBOOTIMG_ARGS) --dt $(INSTALLED_DTIMAGE_TARGET) --output $@
-	@echo -e ${CL_CYN}"Made boot image: $@"${CL_RST}
-
-LZMA_RAMDISK := $(PRODUCT_OUT)/ramdisk-recovery-lzma.img
-
-$(LZMA_RAMDISK): $(recovery_ramdisk)
-	gunzip -f < $(recovery_ramdisk) | lzma > $@
+	$(hide) $(call assert-max-image-size,$@,$(BOARD_BOOTIMAGE_PARTITION_SIZE),raw)
+	@echo "Made boot image: $@"
 
 ## Overload recoveryimg generation: Same as the original, + --dt arg
-$(INSTALLED_RECOVERYIMAGE_TARGET): $(MKBOOTIMG) $(INSTALLED_DTIMAGE_TARGET) $(LZMA_RAMDISK) $(recovery_kernel)
-	@echo -e ${CL_CYN}"----- Making recovery image ------"${CL_RST}
-	$(hide) $(MKBOOTIMG) $(INTERNAL_RECOVERYIMAGE_ARGS) $(BOARD_MKBOOTIMG_ARGS) --dt $(INSTALLED_DTIMAGE_TARGET) --output $@ --ramdisk $(LZMA_RAMDISK)
+$(INSTALLED_RECOVERYIMAGE_TARGET): $(MKBOOTIMG) $(INSTALLED_DTIMAGE_TARGET) \
+		$(recovery_uncompressed_ramdisk) \
+		$(recovery_kernel) \
+		$(MINIGZIP)
+	@echo "----- Making recovery ramdisk ------"
+	$(hide) rm -f $(recovery_uncompressed_ramdisk)
+	$(MKBOOTFS) $(TARGET_RECOVERY_ROOT_OUT) > $(recovery_uncompressed_ramdisk)
+	$(MINIGZIP) < $(recovery_uncompressed_ramdisk) > $(recovery_ramdisk)
+	@echo "----- Making recovery image ------"
+	$(hide) $(MKBOOTIMG) $(INTERNAL_RECOVERYIMAGE_ARGS) $(BOARD_MKBOOTIMG_ARGS) --dt $(INSTALLED_DTIMAGE_TARGET) --output $@
 	$(hide) $(call assert-max-image-size,$@,$(BOARD_RECOVERYIMAGE_PARTITION_SIZE),raw)
-	@echo -e ${CL_CYN}"Made recovery image: $@"${CL_RST}
+	@echo "Made recovery image: $@"
