00852f3d91cd245a703fec05d772e1944d092877
Erfan Abdi <erfangplus@gmail.com>
2017-02-10 13:35:05 +0330

    fix CMACTIONS for DU compatible

--- cmactions/src/com/cyanogenmod/settings/device/BootCompletedReceiver.java
+++ cmactions/src/com/cyanogenmod/settings/device/BootCompletedReceiver.java
@@ -26,8 +26,6 @@ import android.util.Log;
 
 import com.cyanogenmod.settings.device.ServiceWrapper.LocalBinder;
 
-import org.cyanogenmod.internal.util.FileUtils;
-
 public class BootCompletedReceiver extends BroadcastReceiver {
     static final String TAG = "CMActions";
     private ServiceWrapper mServiceWrapper;
@@ -35,17 +33,6 @@ public class BootCompletedReceiver extends BroadcastReceiver {
     @Override
     public void onReceive(final Context context, Intent intent) {
         Log.i(TAG, "Booting");
-
-        // Restore nodes to saved preference values
-        for (String pref : Constants.sButtonPrefKeys) {
-             String value = Constants.isPreferenceEnabled(context, pref) ? "1" : "0";
-             String node = Constants.sBooleanNodePreferenceMap.get(pref);
-
-             if (!FileUtils.writeLine(node, value)) {
-                 Log.w(TAG, "Write to node " + node +
-                       " failed while restoring saved preference values");
-             }
-        }
 
         context.startService(new Intent(context, ServiceWrapper.class));
     }
