b862b59e7cb263a3905f68136c3881075beb9378
Rabih Salamey <somcom3@gmail.com>
2017-06-22 23:20:12 -0400

    griffin: update incorrect import/remove unused overlay
        * derp on the cherry-pick train
    
    Change-Id: I0d157724d516d75a772f28bb244e6b45f0fc9c59

--- cmactions/src/com/cyanogenmod/settings/device/Constants.java
+++ cmactions/src/com/cyanogenmod/settings/device/Constants.java
@@ -24,7 +24,7 @@ import android.content.SharedPreferences;
 import android.preference.PreferenceManager;
 import android.util.Log;
 
-import org.cyanogenmod.internal.util.FileUtils;
+import com.android.internal.util.cr.FileUtils
 
 public class Constants {
 
