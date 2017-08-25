0518419942137915613b24508d4242d5ca9d19d7
Erfan Abdi <erfangplus@gmail.com>
2017-08-25 12:44:06 +0300

    griffin: remove ENABLE_CPUSETS and ENABLE_SCHEDBOOST
    We now have runtime detection.
    
    Signed-off-by: Erfan Abdi <erfangplus@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -48,9 +48,6 @@ TARGET_2ND_CPU_ABI := armeabi-v7a
 TARGET_2ND_CPU_ABI2 := armeabi
 TARGET_2ND_CPU_VARIANT := cortex-a53
 
-# CPUSets
-ENABLE_CPUSETS := true
-
 # Crypto
 TARGET_HW_DISK_ENCRYPTION := true
 
