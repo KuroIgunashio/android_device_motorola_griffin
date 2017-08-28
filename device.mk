d655506396dbb6939781f7d22f41fb1896939441
Rabih Salamey <somcom3@gmail.com>
2017-08-28 19:02:30 -0400

    griffin: more hardware changes to O

--- device.mk
+++ device.mk
@@ -136,9 +136,13 @@ PRODUCT_PACKAGES += \
     android.hardware.graphics.allocator@2.0-service \
     android.hardware.graphics.composer@2.1-impl \
     android.hardware.graphics.mapper@2.0-impl \
+    copybit.msm8996 \
     android.hardware.memtrack@1.0-impl \
     gralloc.msm8996 \
     hwcomposer.msm8996 \
+    memtrack.msm8996 \
+    liboverlay \
+    libtinyxml \
     libgenlock \
     memtrack.msm8996
 
@@ -154,7 +158,8 @@ PRODUCT_PACKAGES += \
 
 # Fingerprint
 PRODUCT_PACKAGES += \
-    android.hardware.biometrics.fingerprint@2.1-service
+    android.hardware.biometrics.fingerprint@2.1-service \
+    fingerprintd
 
 # GNSS HAL
 PRODUCT_PACKAGES += \
@@ -206,7 +211,8 @@ PRODUCT_COPY_FILES += \
 
 # Keymaster HAL
 PRODUCT_PACKAGES += \
-    android.hardware.keymaster@3.0-impl
+    android.hardware.keymaster@3.0-impl \
+    keystore.msm8996
 
 # Lights
 PRODUCT_PACKAGES += \
@@ -320,11 +326,17 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.vr@1.0-impl
 
+# Bluetooth
+PRODUCT_PACKAGES += \
+    android.hardware.bluetooth@1.0-impl \
+    libbt-vendor
+
 # WiFi HAL
 PRODUCT_PACKAGES += \
     android.hardware.wifi@1.0-service
 
 PRODUCT_PACKAGES += \
+    libnfnetlink \
     ipacm \
     ipacm-diag \
     IPACM_cfg.xml \
